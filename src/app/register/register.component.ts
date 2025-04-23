185b36b213b62e6d6c5130c190ae259bef8e897f
Bui Xuan Truong <truongbuixuan11@gmail.com>
2025-04-23 22:56:17 +0700

    validate register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('registerForm') registerForm!: NgForm;
   phoneNumber: string;
   password: string;
   retypePassword: string;
@@ -38,6 +40,34 @@ export class RegisterComponent implements OnInit {
 
   onDateOfBirthChange(){
 
+  }
+  checkPasswordMatch(){
+    if(this.password !== this.retypePassword){
+      this.registerForm.form.controls['retypePassword'].setErrors({'passwordMisMatch': true});
+    } else{
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      if (birthDate > today) {
+        this.registerForm.controls['dateOfBirth'].setErrors({ 'futureDate': true });
+        return;
+      }
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      if (age < 18) {
+        this.registerForm.controls['dateOfBirth'].setErrors({ 'invalidAge': true });
+      } else {
+        this.registerForm.controls['dateOfBirth'].setErrors(null);
+      }
+    }
   }
 
 }
